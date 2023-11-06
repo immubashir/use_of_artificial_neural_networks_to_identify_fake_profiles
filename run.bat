cbfd1dca90320c276808f5b449176dae310811ec
immubashir <52886949+immubashir@users.noreply.github.com>
2023-11-06 14:08:52 +0530

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,35 @@
+python -m pip install --upgrade pip
+pip install numpy==1.19.2
+pip install matplotlib==3.1.3 
+pip install pandas==0.25.3 
+pip install opencv-python==4.2.0.32
+pip install keras==2.3.1 
+pip install tensorflow==1.14.0 
+pip install h5py==2.10.0 
+pip install protobuf==3.16.0
+pip install pillow==7.0.0
+pip install sklearn-genetic==0.2
+pip install SwarmPackagePy
+pip install sklearn
+pip install scikit-learn==0.22.2.post1
+Pip install sklearn-extensions==0.0.2
+Pip install pyswarms==1.1.0
+pip install nltk
+pip install pyaes
+pip install pbkdf2
+Pip install opencv-contrib-python==4.3.0.36
+pip install django==2.1.7
+pip install pymysql==0.9.3
+pip uninstall crypto
+pip uninstall pycryptodome
+pip install pycryptodome
+pip install pycryptodome==3.10.1
+pip install   cryptography==2.9.2
+pip install imutils	
+pip install seaborn
+pip install urllib3==1.26.6 
+
+pause
+
+
+
